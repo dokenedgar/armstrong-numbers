aad29906f3f6a487d79c73592e4cb2441cf771b4
Doken Edgar <dokenedgarjr@gmail.com>
2025-01-22 16:02:44 +0100

    Solve armstrong number exercise

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let mut total = 0;
+    let length = num.to_string().len() as u32;
+    for item in num.to_string().chars() {
+        total += item.to_digit(10).unwrap().pow(length);
+    }
+    total == num
 }
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let num: u32 = 153;
+    armstrong_numbers::is_armstrong_number(num);
+}
